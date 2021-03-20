d0040bd43ea744a28b52d7abbc2ca741cd61d74c
Daniel Goodwin <dcg2112@ad.unc.edu>
2021-03-20 12:29:18 -0400

    Complete workshop exersizes. Cheated a bit on the search bar...

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,11 +1,75 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const sizes = {
+  small: {
+    height: "8",
+    radius: "4",
+    padding: "0",
+  },
+  medium: {
+    height: "12px",
+    radius: "4",
+    padding: "0",
+  },
+  large: {
+    height: "16",
+    radius: "8",
+    padding: "4",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = sizes[size];
+
+  if (!styles) {
+    throw new Error(`Size value is not valid`);
+  }
+
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        "--border-radius": styles.radius + "px",
+        "--container-padding": styles.padding + "px",
+      }}
+    >
+      <ProgressWrapper>
+        <Progress
+          style={{
+            width: value + "%",
+            height: styles.height + "px",
+            padding: styles.padding + "px",
+          }}
+        />
+      </ProgressWrapper>
+      <VisuallyHidden>{value}</VisuallyHidden>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  border-radius: var(--border-radius);
+  padding: var(--container-padding);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  overflow: hidden;
+`;
+
+const ProgressWrapper = styled.div`
+  overflow: hidden;
+  border-radius: 4px;
+`;
+
+const Progress = styled.div`
+  border-radius: 4px 0 0 4px;
+  color: ${COLORS.primary};
+  background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,66 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationLayer>
+        {displayedValue}
+        <IconWrapper style={{"--icon-size": 24 + "px"}}>
+          <Icon id="chevron-down" width={24}/>
+        </IconWrapper>
+      </PresentationLayer>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const PresentationLayer = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  padding: 12px 52px 12px 16px;
+  border: none;
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+  ${NativeSelect}:focus + & {
+    border-radius: 0;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`;
+const IconWrapper = styled.div`
+  display: inline-block;
+  position: absolute;
+  right: 6px;
+  top: 0;
+  bottom: 0;
+  height: var(--icon-size);
+  width: var(--icon-size);
+  margin-block: auto;
+  pointer-events: none;
+`;
 
 export default Select;
